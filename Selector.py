ec8c71351a16200588647d8d781c8c18342cbbdc
SevenB1 <qambarmadad12@gmail.com>
2023-07-16 06:19:05 -0700

    2

---
+++ Selector.py
@@ -0,0 +1,14 @@
+print ("What can I do for you?")
+print ("1.Calculator")
+print ("2.Grading")
+print ("3.Parity")
+print ("4.Comparison") 
+
+def Calculator():
+
+ x = float(input ("what's x?"))
+ y = float(input ("what's y?"))
+
+ z = ( x + y)
+
+ print(f"{z}")
