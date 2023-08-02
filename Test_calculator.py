ed572f92dbbef86efed2fb201272ce1e848667f7
SevenB1 <qambarmadad12@gmail.com>
2023-08-02 21:12:03 +0500

    Testing the square calculator by using pytest.

---
+++ Test_calculator.py
@@ -0,0 +1,9 @@
+from Square_Calculator import square
+
+def test_square():
+    assert square(2) == 4
+    assert square(3) == 9
+    assert square(-2) == 4
+    assert square(-3) == 9
+    assert square(0) == 0
+  
