9b9ced948308fb04a1afbba9d9ebc64ef4dccf5c
SevenB1 <qambarmadad12@gmail.com>
2023-08-23 18:11:31 +0500

    Update.

--- Names.py
+++ Names.py
@@ -1,4 +1,8 @@
-with open("names.txt", "r") as file:
-    lines = file.readlines()
-for line in lines:
-    print("Hello", line.rstrip())
+names = []
+
+with open("names.txt") as file:
+    for line in file:
+        names.append(line.rstrip())
+
+for name in sorted(names):
+    print(f"Hello, {name}")
