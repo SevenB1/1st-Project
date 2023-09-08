bcb9065d7cbc1d385a88b5ca8a2bc5ffa57d12ef
SevenB1 <qambarmadad12@gmail.com>
2023-09-09 00:35:53 +0500

    Update.

--- Student.py
+++ Student.py
@@ -1,13 +1,14 @@
 def main():
     student = get_student()
-    if student[0] == "Padma":
-        student[1] = "Ravenclaw"
-    print(f"{student[0]} from {student[1]}")
+    if student["name"] == "Padma":
+        student["house"] == "Ravenclaw"
+    print(f"{student['name']} from {student['house']}")
 
 def get_student():
-    name = input("Name: ")
+    name= input("Name: ")
     house = input("House: ")
-    return [name, house] 
-    
+    return {"name": name, "house": house} 
+
+
 if __name__=="__main__":
     main()
