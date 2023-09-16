e7c3b55b09fe482dbfa5d06a497a5f46382b7353
SevenB1 <qambarmadad12@gmail.com>
2023-09-16 17:21:46 +0500

    THe formula to calculate distance.

---
+++ Distance Formula.py
@@ -0,0 +1,6 @@
+vi = float(input("What is the initial velocity? "))
+vf = float(input("What is the final velocity? "))
+a = float(input("What's is acceleration? "))
+s = (vf**2 - vi **2)/(2*a)
+
+print (f"The total distance is {s} metres.")
