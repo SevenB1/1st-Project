0416751c23086f8c1f215f787563f0169697d90d
SevenB1 <qambarmadad12@gmail.com>
2023-09-20 23:19:52 +0500

    .

---
+++ NEXUS_Loops.py
@@ -0,0 +1,15 @@
+#import random 
+
+# import math 
+# math.pi
+#r = 1
+# for r in range(1,11):
+#     area = 3.14 * r ** 2
+#     print(f"The area of a circle with radius {r} is {area}")
+#     r += 1
+base = 4
+for height in range(1,11):
+    area = 1/2 * base * height
+    print(f"The area of triangle with base 4cm and height {height}cm is area {area}")
+
+#Menu of area of circle, triangle, Cylinder, rectangle, Square
