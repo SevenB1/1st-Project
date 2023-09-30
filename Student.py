997b4608c4513bf0f7fa4b67afd54590d24b9fe2
SevenB1 <qambarmadad12@gmail.com>
2023-09-30 19:03:14 +0500

    Used properties.

--- Student.py
+++ Student.py
@@ -1,33 +1,36 @@
 class Student:
-    def __init__(self, name, house, patronus):
-        if not name:
-            raise ValueError("Missing name")
-        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
-            raise ValueError("Invalid House")
+    def __init__(self, name, house):
         self.name = name
         self.house = house
-        self.patronus = patronus
 
     def __str__(self):
         return f"{self.name} from {self.house}"
-    def charm(self):
-        match self.patronus:
-            case "Stag":
-                return "Reindeer"
-            case "Otter":
-                return "Racoon"
-            case _:
-                return "none"
+    
+    @property
+    def name(self):
+        return self._name
+    
+    def name(self, name):
+        if not name:
+            raise ValueError("Missing Name")
+        self._name = name
+
+    @property
+    def house(self):
+        return self._house
+    @house.setter    
+    def house(self, house):
+        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+            raise ValueError("Invalid house")    
+        self._house = house
 def main():
     student = get_student()
-    print("Expecto Petronus!")
-    print(student.charm())
+    print(student)
 
 def get_student():
     name = input("Name: ")
     house = input("House: ")
-    patronus = input("Patronus: ")
-    return Student(name, house, patronus)
+    return Student(name, house)
     
 if __name__=="__main__":
     main() 
